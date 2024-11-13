4b03ecd19f2ee1067def432eb0f6ddda9dfa1526
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-11-14 06:36:57 +0700

    feat - retrieves the battery for use in the electric engine.

--- src/main/java/com/cargigafactory/engine/BatteryManagementSystem.java
+++ src/main/java/com/cargigafactory/engine/BatteryManagementSystem.java
@@ -1,4 +1,18 @@
 package com.cargigafactory.engine;
 
+import com.cargigafactory.storage.Battery;
+
+/**
+ * Manages the battery in an electric vehicle.
+ */
 public class BatteryManagementSystem {
+    /**
+     * Retrieves the battery for use in the electric engine.
+     *
+     * @return a Battery instance
+     */
+    public Battery getBattery() {
+        System.out.println("BatteryManagementSystem providing battery...");
+        return new Battery();
+    }
 }
