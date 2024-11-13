4d417ea4a9587157ea4f555602a9ee6b67b2c077
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-11-14 06:37:07 +0700

    feat - electric engine for a vehicle.

--- src/main/java/com/cargigafactory/engine/ElectricEngine.java
+++ src/main/java/com/cargigafactory/engine/ElectricEngine.java
@@ -1,4 +1,18 @@
 package com.cargigafactory.engine;
 
-public class ElectricEngine {
+import com.cargigafactory.storage.Battery;
+
+/**
+ * Represents an electric engine for a vehicle.
+ */
+public class ElectricEngine implements EngineInterface {
+    private BatteryManagementSystem batteryManagementSystem = new BatteryManagementSystem();
+    private ElectricMotor motor = new ElectricMotor();
+
+    @Override
+    public void moveVehicle() {
+        System.out.println("Using electric engine to move the vehicle...");
+        Battery battery = batteryManagementSystem.getBattery();
+        motor.rotateElectricMotor(battery);
+    }
 }
