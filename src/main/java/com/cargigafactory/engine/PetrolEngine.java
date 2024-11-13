7a1fe57ddf758e95d25fe0dd50b46ff3eedd79b7
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-11-14 06:37:15 +0700

    feat - petrol engine for a vehicle.

--- src/main/java/com/cargigafactory/engine/PetrolEngine.java
+++ src/main/java/com/cargigafactory/engine/PetrolEngine.java
@@ -1,4 +1,16 @@
 package com.cargigafactory.engine;
 
-public class PetrolEngine {
+/**
+ * Represents a petrol engine for a vehicle.
+ */
+public class PetrolEngine implements EngineInterface {
+    private OilPipe oilPipe = new OilPipe();
+    private Piston piston = new Piston();
+
+    @Override
+    public void moveVehicle() {
+        System.out.println("Using petrol engine to move the vehicle...");
+        oilPipe.suckOil();
+        piston.movePiston();
+    }
 }
