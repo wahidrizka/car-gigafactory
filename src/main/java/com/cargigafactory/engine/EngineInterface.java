81b09d9423d3d6eedc01e27a51bcc0682b923683
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-11-14 06:27:24 +0700

    feat - moves the vehicle using the engine.

--- src/main/java/com/cargigafactory/engine/EngineInterface.java
+++ src/main/java/com/cargigafactory/engine/EngineInterface.java
@@ -1,4 +1,11 @@
 package com.cargigafactory.engine;
 
+/**
+ * Interface representing a generic engine.
+ */
 public interface EngineInterface {
+    /**
+     * Moves the vehicle using the engine.
+     */
+    void moveVehicle();
 }
