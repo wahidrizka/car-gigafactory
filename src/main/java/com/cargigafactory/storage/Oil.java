23e8068cee1c8966b6ffb05a50679f74ae3df033
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-11-14 06:11:12 +0700

    feat - Fills the storage with oil.

--- src/main/java/com/cargigafactory/storage/Oil.java
+++ src/main/java/com/cargigafactory/storage/Oil.java
@@ -1,4 +1,13 @@
 package com.cargigafactory.storage;
 
+/**
+ * Represents oil used as a fiel in a petrol engine.
+ */
 public class Oil {
+    /**
+     * Fills the storage with oil.
+     */
+    public void fill() {
+        System.out.println("Filling oil...");
+    }
 }
