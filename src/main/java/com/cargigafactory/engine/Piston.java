a0a58bf3395680cdd7c1606228eb1ea89580bb71
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-11-14 06:13:06 +0700

    feat - Moves the piston as part of the engine's operation.

--- src/main/java/com/cargigafactory/engine/Piston.java
+++ src/main/java/com/cargigafactory/engine/Piston.java
@@ -1,4 +1,13 @@
 package com.cargigafactory.engine;
 
+/**
+ * Represents a piston in a petrol engine.
+ */
 public class Piston {
+    /**
+     * Moves the piston as part of the engine's operation.
+     */
+    public void movePiston() {
+        System.out.println("Piston moving...");
+    }
 }
