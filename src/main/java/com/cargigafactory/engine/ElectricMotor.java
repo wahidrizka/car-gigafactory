650c1df4971b51f41d342ddad0564230ec36d909
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-11-14 06:22:03 +0700

    feat - Rotates the motor using battery power.

--- src/main/java/com/cargigafactory/engine/ElectricMotor.java
+++ src/main/java/com/cargigafactory/engine/ElectricMotor.java
@@ -1,4 +1,17 @@
 package com.cargigafactory.engine;
 
+import com.cargigafactory.storage.Battery;
+
+/**
+ * Represents an electric motor that uses battery power to rotate.
+ */
 public class ElectricMotor {
+    /**
+     * Rotates the motor using battery power.
+     *
+     * @param battery The battery providing power to the motor.
+     */
+    public void rotateElectricMotor(Battery battery) {
+        System.out.println("ElectricMotor rotating using battery power...");
+    }
 }
