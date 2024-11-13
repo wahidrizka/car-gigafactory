67a034dd7d2e6db99ada88d4c5eb587be6201a93
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-11-14 06:11:04 +0700

    feat - Charges the energy storage.

--- src/main/java/com/cargigafactory/storage/Energy.java
+++ src/main/java/com/cargigafactory/storage/Energy.java
@@ -1,4 +1,13 @@
 package com.cargigafactory.storage;
 
+/**
+ * Represents energy used in electric systems.
+ */
 public class Energy {
+    /**
+     * Charges the energy storage.
+     */
+    public void charge() {
+        System.out.println("Charging energy...");
+    }
 }
