4baff1f19b4f5a1e9b7a3bc58005388c55d5ef29
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-11-14 06:03:23 +0700

    feat - creates a flow of electrons.

--- src/main/java/com/cargigafactory/storage/Electrons.java
+++ src/main/java/com/cargigafactory/storage/Electrons.java
@@ -1,4 +1,13 @@
 package com.cargigafactory.storage;
 
+/**
+ * Represents electrons flowing in an electric engine.
+ */
 public class Electrons {
+    /**
+     * Creates a flow of electrons.
+     */
+    public void flowOfElectrons() {
+        System.out.println("Electrons flowing...");
+    }
 }
