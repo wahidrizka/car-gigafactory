684b3b36dbd447b8f72d8918f5df0d11cf8d09e0
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-11-14 06:14:24 +0700

    feat - Sucks oil from the tank to the engine.

--- src/main/java/com/cargigafactory/engine/OilPipe.java
+++ src/main/java/com/cargigafactory/engine/OilPipe.java
@@ -1,4 +1,13 @@
 package com.cargigafactory.engine;
 
+/**
+ * Represents an oil pipe used in a petrol engine.
+ */
 public class OilPipe {
+    /**
+     * Sucks oil from the tank to the engine.
+     */
+    public void suckOil() {
+        System.out.println("OilPipe sucking oil...");
+    }
 }
