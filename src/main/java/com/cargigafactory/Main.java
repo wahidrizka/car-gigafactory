ab7d94424222321fb8439ce205f37cef55da976a
whdrzka <58039870+wahidrizka@users.noreply.github.com>
2024-11-14 07:05:43 +0700

    feat - Main method to run the Car Gigafactory application.

--- src/main/java/com/cargigafactory/Main.java
+++ src/main/java/com/cargigafactory/Main.java
@@ -1,4 +1,18 @@
 package com.cargigafactory;
 
+import com.cargigafactory.cli.CarCLI;
+
+/**
+ * The main entry point of the application.
+ */
 public class Main {
+    /**
+     * Main method to run the Car Gigafactory application.
+     *
+     * @param args The command line arguments.
+     */
+    public static void main(String[] args) {
+        CarCLI cli = new CarCLI();
+        cli.run();
+    }
 }
